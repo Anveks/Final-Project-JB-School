e3300a779aea2767552d99e14874b21e406a1914
Anveks <vekselmana@gmail.com>
2023-05-20 16:46:48 +0300

    style improvements

--- Frontend/src/Components/AuthArea/Login/Login.tsx
+++ Frontend/src/Components/AuthArea/Login/Login.tsx
@@ -4,6 +4,10 @@ import CredentialsModel from "../../../Models/CredentialsModel";
 import authService from "../../../Services/AuthService";
 import notifyService from "../../../Services/NotifyService";
 import "./Login.css";
+import beach from "../../../assets/img/beach.jpg";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Login(): JSX.Element {
 
@@ -24,13 +28,25 @@ function Login(): JSX.Element {
     return (
         <div className="Login">
 
+            <img src={beach} alt="beach" />
+            <div className="welcome-text">
+                <div>ARE YOU READY FOR YOUR NEXT VACATION?</div>
+                <p>
+                    <FacebookIcon fontSize="large" />
+                    <TwitterIcon fontSize="large" />
+                    <InstagramIcon fontSize="large" />
+                </p>
+
+                <p className="about">Welcome to our Travel Adventure! Explore breathtaking destinations, immerse yourself in vibrant cultures, and create unforgettable memories. Start your journey with us today!</p>
+            </div>
+
             <form onSubmit={handleSubmit(submit)}>
                 <h2 className="title">Login</h2>
                 <label>Email:</label>
-                <input type="email" {...register("email")} required minLength={4} maxLength={20} autoFocus />
+                <input type="email" {...register("email")} placeholder="example@mail.com" required minLength={4} maxLength={20} autoFocus />
 
                 <label>Password:</label>
-                <input type="password" {...register("password")} required minLength={4} maxLength={20} />
+                <input type="password" {...register("password")} placeholder="******" required minLength={4} maxLength={20} />
 
                 <button>Log in</button>
 
